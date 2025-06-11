b8e7540b15216390629c990e779f6010d0663a65
Zakaria REGUIEG <98493579+Zvki1@users.noreply.github.com>
2025-06-11 10:22:48 +0200

    Update Main.java
    add * /

--- Main.java
+++ Main.java
@@ -6,6 +6,12 @@ class Main {
       else if(args[(args.length-1)].equals("-")){
         System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
       }
+      else if(args[(args.length-1)].equals("*")){
+        System.out.println(Integer.parseInt(args[0])*Integer.parseInt(args[1]));
+      }
+      else if(args[(args.length-1)].equals("/")){
+        System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));
+      }
       else {System.exit(-1);}
     }
 }
